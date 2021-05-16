786dd7060cf4665085c3a73f8403f050f81a7551
Shubhankar <shubhankar.nautiyal21@gmail.com>
2021-05-16 12:07:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: MyHomePage(title: ' Money heist'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+            style: TextStyle(
+              fontFamily: 'Shelter',
+              fontSize: 40,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/images/MH1.jpg'),
+              Text(
+                '\nMoney Heist',
+                style: TextStyle(
+                  color: Color.fromARGB(1000, 232, 69, 69),
+                  fontFamily: 'Shelter',
+                  // fontStyle: FontStyle.italic,
+                  fontSize: 80,
+                ),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Color.fromRGBO(21, 21, 21, 1)
+
+        // Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     Container(
+        //       height: 80,
+        //       width: 80,
+        //       color: Colors.purple,
+        //     ),
+        //     Row(
+        //       mainAxisAlignment: MainAxisAlignment.center,
+        //       children: [
+        //         Container(
+        //           height: 80,
+        //           width: 80,
+        //           color: Colors.green,
+        //         ),
+        //         Container(
+        //           height: 80,
+        //           width: 80,
+        //           color: Colors.yellow,
+        //         ),
+        //         Container(
+        //           height: 80,
+        //           width: 80,
+        //           color: Colors.orange,
+        //         ),
+        //       ],
+        //     ),
+        //     Container(
+        //       height: 80,
+        //       width: 80,
+        //       color: Colors.blue,
+        //     )
+        //   ],
+        );
+  }
+}
